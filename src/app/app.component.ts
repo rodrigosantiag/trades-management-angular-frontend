d266a03e777538d1d1113bab74003cd6c6fb1b44
Rodrigo Santiago <rodrigosantiag@gmail.com>
2019-05-22 03:58:49 -0300

    Changing module title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'binary-financial-system-frontend';
+  title = 'Binary Financial Management System';
 }
