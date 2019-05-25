17e260661202bd326c30c3fc6d644f52a3235682
Rodrigo Santiago <rodrigosantiag@gmail.com>
2019-05-25 13:39:41 -0300

    Installing FontAwesome Angular Package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,15 @@ import {SignInComponent} from './sign-in/sign-in.component';
 import {SignUpFormComponent} from './sign-up-form/sign-up-form.component';
 import {ReactiveFormsModule} from '@angular/forms';
 
+// FontAwesome
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+import {far} from '@fortawesome/free-regular-svg-icons';
+import {fab} from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, far, fab);
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,6 +40,7 @@ import {ReactiveFormsModule} from '@angular/forms';
     }),
     AppRoutingModule,
     BrowserModule,
+    FontAwesomeModule,
     HttpClientModule,
     NgbModule,
     ReactiveFormsModule
