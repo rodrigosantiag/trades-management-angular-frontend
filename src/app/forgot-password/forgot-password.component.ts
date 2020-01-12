f284a955de7ddd8f7379ab7a768e9e5efb43b292
Rodrigo Santiago <rodrigosantiag@gmail.com>
2020-01-12 12:19:49 -0300

    Starting development of forgot-password.component.ts

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormUtils} from '../shared/form.utils';
+import {FlashMessagesService} from '../shared/flashMessages.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+  public form: FormGroup;
+  public submitted: boolean;
+  public formUtils: FormUtils;
+  public messages: Array<string>;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private flashMessage: FlashMessagesService) {
+    this.setUpForm();
+    this.formUtils = new FormUtils(this.form);
+    this.submitted = false;
+    this.messages = null;
+  }
 
   ngOnInit() {
+  }
+
+  private setUpForm() {
+    this.form = this.formBuilder.group({
+      login: [null, [Validators.required, Validators.email]]
+    });
   }
 
 }
