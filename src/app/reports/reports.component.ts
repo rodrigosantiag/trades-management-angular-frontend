8c81487665946059a57ef8595a1eeaf46c1c11f7
Rodrigo Santiago <rodrigo.santiago@noverde.com.br>
2020-09-07 21:04:00 -0300

    creating filter form fields for trade reports

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountService} from '../accounts/shared/account.service';
+import {Account} from '../accounts/shared/account.model';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {Strategy} from '../strategies/shared/strategy.model';
+import {StrategyService} from '../strategies/shared/strategy.service';
 
 @Component({
   selector: 'app-reports',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  public maxDate: Date;
+  public accounts: Array<Account>;
+  public strategies: Array<Strategy>;
+
+  constructor(private accountService: AccountService, private flashMessagesService: FlashMessagesService,
+              private strategyService: StrategyService) {
+
+    this.maxDate = new Date();
+
+  }
 
   ngOnInit(): void {
+    this.accountService.getAll()
+      .subscribe(accounts => this.accounts = accounts,
+        () => {
+          this.flashMessagesService.show(
+            'Something went wrong. Please refresh page.',
+            {
+              cssClass: 'alert-danger',
+              timeout: 5000
+            });
+        });
+
+    this.strategyService.getAll()
+      .subscribe(strategies => this.strategies = strategies,
+        () => {
+          this.flashMessagesService.show(
+            'Something went wrong. Please refresh page.',
+            {
+              cssClass: 'alert-danger',
+              timeout: 5000
+            });
+        });
   }
 
 }
